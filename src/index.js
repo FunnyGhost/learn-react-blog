398f5dbd7d1423eaea78f4adf9f0e6234a1fc4b6
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-09-20 19:40:28 +0200

    Setup redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <App/>
     </Provider>,
     document.querySelector('#root')
 );
+
